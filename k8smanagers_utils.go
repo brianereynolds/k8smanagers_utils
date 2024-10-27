26564bd4a9524240c9791ef4445a1d5814a72781
Brian Reynolds <breyno11@its.jnj.com>
2024-10-27 20:05:45 +0000

    Export functions

--- k8smanagers_utils.go
+++ k8smanagers_utils.go
@@ -10,10 +10,10 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/containerservice/armcontainerservice"
 )
 
-// compareVersions will compare the versions by breaking them down into major, minor, and patch components
+// CompareVersions will compare the versions by breaking them down into major, minor, and patch components
 // They are then converting them to integers, and comparing them in sequence.
 // It returns 1 when version1 is greater than version2, -1 when version1 is less than version2 and 0 when they are equal
-func compareVersions(version1, version2 string) (int, error) {
+func CompareVersions(version1, version2 string) (int, error) {
 
 	v1Parts := strings.Split(version1, ".")
 	v2Parts := strings.Split(version2, ".")
@@ -37,9 +37,9 @@ func compareVersions(version1, version2 string) (int, error) {
 	return 0, nil
 }
 
-// getContainerServiceClient will create a Container Service Client
-func getManagedClusterClient(ctx context.Context, sub string) (*armcontainerservice.ManagedClustersClient, error) {
-	cred, err := getAzureCredential(ctx)
+// GetManagedClusterClient will create a Container Service Client
+func GetManagedClusterClient(ctx context.Context, sub string) (*armcontainerservice.ManagedClustersClient, error) {
+	cred, err := GetAzureCredential(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -52,8 +52,8 @@ func getManagedClusterClient(ctx context.Context, sub string) (*armcontainerserv
 	return managedClustersClient, nil
 }
 
-// getAzureCredential will create a new Azure Credential
-func getAzureCredential(ctx context.Context) (*azidentity.DefaultAzureCredential, error) {
+// GetAzureCredential will create a new Azure Credential
+func GetAzureCredential(ctx context.Context) (*azidentity.DefaultAzureCredential, error) {
 	cred, err := azidentity.NewDefaultAzureCredential(nil)
 	if err != nil {
 		return nil, err
